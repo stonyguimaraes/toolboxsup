979e3762950e3f2d0cdacd787a862371c1819d52
Stony Gomes Guimarães <69357791+stonyguimaraes@users.noreply.github.com>
2025-09-07 14:39:26 -0300

    Add files via upload

---
+++ config/Database.php
@@ -0,0 +1,25 @@
+<?php
+class Database
+{
+    private $host = "127.0.0.1";
+    private $db_name = "db_toolboxsup";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Erro na conexão: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
